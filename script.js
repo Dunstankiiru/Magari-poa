493a9954248643389ed8c9c12a61624ef5e12f44
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-30 12:29:15 +0300

    script Dom

--- script.js
+++ script.js
@@ -110,6 +110,9 @@ document.addEventListener("DOMContentLoaded", () => {
             <p><strong>Seats:</strong> ${newCar.numberOfSeats}</p>
             <p><strong>Previous Owners:</strong> ${newCar.previousOwners}</p>
             <p><strong>Color:</strong> ${newCar.color}</p>
+            <h2>${newCar.status === "sold" ? "<span class='sold'>SOLD</span>" : ""}</h2>
+            ${newCar.status !== "sold" ? `<button class="btn btn-outline-success" data-id="${newCar.id}">Buy Now</button>` : ""}
+            <p class="car-id" style="display:none;">${newCar.id}</p>
         `;
 
         carContainer.appendChild(carCard);
